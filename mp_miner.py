c9d4196d57e148b31eedecd2f5ef96a5378ec238
Kenny Peng <knypng44@gmail.com>
2020-12-21 10:58:17 -0800

    Remove accepted and rejected shares counter

--- mp_miner.py
+++ mp_miner.py
@@ -12,7 +12,7 @@ pool_port = int(pool_location[1])
 
 job_request_bytes = ('JOB,'+USERNAME).encode('utf-8')
 
-def mineDUCO(hashcount, accepted, rejected):
+def mineDUCO(hashcount):
     soc = socket.socket()
     try:
         soc.connect((pool_ip, pool_port))
@@ -27,14 +27,8 @@ def mineDUCO(hashcount, accepted, rejected):
             result = nonceMiner.c_mine_DUCO_S1(prefix_bytes, target_bytes, difficulty)
 
             soc.send(str(result).encode('utf-8')) # Send result of hashing algorithm to pool
-            feedback = soc.recv(1024).decode() # Get feedback about the result
-
-            if feedback == 'GOOD':
-                with accepted.get_lock():
-                    accepted.value += 1
-            elif feedback == 'BAD':
-                with rejected.get_lock():
-                    rejected.value += 1
+            soc.recv(1024) # Receive feedback, don't bother decoding
+            
             with hashcount.get_lock():
                 hashcount.value += result
     except:
@@ -43,11 +37,9 @@ def mineDUCO(hashcount, accepted, rejected):
 
 if __name__ == '__main__':
     hashcount = mp.Value('i', 0, lock=True)
-    accepted = mp.Value('i', 0, lock=True)
-    rejected = mp.Value('i', 0, lock=True)
     p_list = []
     for i in range(N_PROCESSES):
-        p = mp.Process(target=mineDUCO, args=tuple([hashcount, accepted, rejected]))
+        p = mp.Process(target=mineDUCO, args=tuple([hashcount]))
         p.start()
         p_list.append(p)
         time.sleep(1/N_PROCESSES)
@@ -55,27 +47,20 @@ if __name__ == '__main__':
         past_time = time.time()
         while True:
             time.sleep(2)
-            current_time = time.time()
 
             with hashcount.get_lock():
                 hash_in_2s = hashcount.value
                 hashcount.value = 0
-            with accepted.get_lock():
-                accepted_in_2s = accepted.value
-                accepted.value = 0
-            with rejected.get_lock():
-                rejected_in_2s = rejected.value
-                rejected.value = 0
-            
-            print('Hash rate: %.2f' % (hash_in_2s/1000000/(current_time-past_time)), 'MH/s')
-            print('Accepted shares in 2s:', accepted_in_2s, '\tRejected shares in 2s:', rejected_in_2s)
-            print()
+            current_time = time.time()
+            hashrate = hash_in_2s/1000000/(current_time-past_time)
             past_time = current_time
+            
+            print('Hash rate: %.2f MH/s' % hashrate)
 
             for i in range(len(p_list)):
                 if(not p_list[i].is_alive()):
                     p_list[i].join()
-                    p_list[i] = mp.Process(target=mineDUCO, args=tuple([hashcount, accepted, rejected]))
+                    p_list[i] = mp.Process(target=mineDUCO, args=tuple([hashcount]))
                     p_list[i].start()
     except:
         print('Terminating processes...')
